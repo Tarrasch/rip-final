cc721dfa684ea938921c94385e04268b1d2e9138
Harrison Jones <bitbucket@hhj.me>
2012-12-10 00:23:24 -0500

    Linear predector now averages data points

--- LinearPredictor.cpp
+++ LinearPredictor.cpp
@@ -14,20 +14,71 @@ using namespace std;
 using namespace Eigen;
 
 LinearPredictor::LinearPredictor(std::list<Eigen::VectorXd> observedPath, double time){
-        assert(observedPath.size() > 0);
-        VectorXd last = observedPath.back();
-        list<VectorXd>::iterator it = observedPath.end();
-        it--;
-        if(it != observedPath.begin()) { // In case we only have one single element!
-          it--;
-        }
-        VectorXd beforeLast = *it;
-    
-        //call projectileMotionWRandT with no randomness
-        VectorXd vel = (last - beforeLast)/dt;
-        VectorXd acc(3); acc<<0,0,0;
+	int pathSize = observedPath.size();
+	
+	// Only continue if the observed path is greater than 0
+	assert(pathSize > 0);
+	
+	// Grab the last point in the path
+	VectorXd p2(3);	// = observedPath.back();
+	p2 < 0,0,0;
+	VectorXd p1(3); //= *it;
+	p1 < 0,0,0;
+	
+	// Build an iterator over the path list
+	list<VectorXd>::iterator it = observedPath.end();
+	it--;
+	
+	// If there is only 1 path point (occurs at the very "start" of time.) just use that one point
+	if (pathSize == 1)
+	{
+		p1 = *it;
+	}
+	else if (pathSize == 2)
+	{
+		it--;
+		p1 = *it;
+	}
+	else if (pathSize == 3)
+	{
+		it--;
+		p2 += *it;
+		p2 = p2 * (1/2);
+		it--;
+		p1 = *it;
+	}
+	else
+	{
+		int i = pathSize;
+		while (i > pathSize/2)
+		{
+			p2 += *it;
+			it--;
+			i--;
+		}
+		p2 = p2 * (2/pathSize);
 
-        predictedPath = projectileMotionWRandT(beforeLast, vel, acc, 0, time);
+		while (i > 0)
+		{
+			p1 += *it;
+			it--;
+			i--;
+		}
+		p1 = p1 * (2/pathSize);
+	}
+
+	//call projectileMotionWRandT with no randomness
+	if (pathSize == 1)
+	{
+		VectorXd vel = (p2 - p1)/dt;
+	}
+	else
+	{
+		VectorXd vel = (p2 - p1)/((pathSize-1)*dt);
+	}
+	VectorXd acc(3); acc<<0,0,0;
+
+	predictedPath = projectileMotionWRandT(p2, vel, acc, 0, time);
 }
 
 std::list<Eigen::VectorXd> LinearPredictor::getPredictedPath(){
